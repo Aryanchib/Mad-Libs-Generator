32ff1b8d72727fdbbc301b5b5024e49a20d89d80
Aryanchib <112257850+Aryanchib@users.noreply.github.com>
2022-08-27 18:29:13 +0530

    First Commit

---
+++ Madlibsgames.py
@@ -0,0 +1,7 @@
+first = input("enter the color")
+sec = input("enter the sec color")
+thrd = input("enter the name of lover")
+
+print("roses are " + first)
+print("violet is " + sec)
+print("i love " + thrd)
